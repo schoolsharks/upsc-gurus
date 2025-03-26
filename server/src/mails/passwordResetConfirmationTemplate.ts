a0ec056f80ca06a27d8248a1d4a07566254fb952
Manish <[manishbulchandani4@gmail.com]>
2025-03-26 16:29:07 +0530

    email template fixed

--- server/src/mails/passwordResetConfirmationTemplate.ts
+++ server/src/mails/passwordResetConfirmationTemplate.ts
@@ -1,20 +1,20 @@
 export const passwordResetConfirmationTemplate = (email: string, firstName: string, newPassword: string) => {
   return `
-Hello ${firstName},
+Hello ${firstName},<br/><br/>
 
-We wanted to let you know that your UPSC Gurus account password has been successfully reset.
+We wanted to let you know that your UPSC Gurus account password has been successfully reset.<br/>
 
-Email ID: ${email}  
-Your new password is: ${newPassword}  
+Email ID: ${email}<br/>
+Your new password is: ${newPassword}<br/><br/>
 
-If you did not request this password reset, please contact us immediately.  
+If you did not request this password reset, please contact us immediately. <br/><br/> 
 
-To access your UPSC Gurus account, click the link below:  
-https://app.upscgurus.in  
+To access your UPSC Gurus account, click the link below:<br/>  
+<a href="https://app.upscgurus.in">https://app.upscgurus.in</a> <br/><br/>
 
-For any issues or questions, feel free to reach out to us at: <a href="enquiry@upscgurus.in">enquiry@upscgurus.in</a>  
-
-Best regards,  
+For any issues or questions, feel free to reach out to us at: <a href="enquiry@upscgurus.in">enquiry@upscgurus.in</a> <br/> 
+<br/>
+Best regards,<br/>  
 UPSC Gurus Team  
   `;
 };
